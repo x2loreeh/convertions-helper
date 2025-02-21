f5bbd0055def3645f31c571f3a06c2be37753ca8
x2loreeh <loreytbs33@gmail.com>
2025-02-21 21:41:53 +0100

    fixed bugs + readme + added more convertions

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-// Emoji Converter + fileSaving
+// Emoji Converter + Salvataggio su file
 function addEmojiConverter(value, input, unitFrom, unitTo) {
     return {
         value: value,
@@ -16,56 +16,106 @@ function addEmojiConverter(value, input, unitFrom, unitTo) {
     };
 }
 
-// Time Conversions
-function minuteToSeconds(minute) {
-    return addEmojiConverter(minute * 60, minute, "min", "sec");
+// ⏳ Time Conversions
+function weekToDay(week) {
+    return addEmojiConverter(week * 7, week, "week", "day");
 }
 
-function secondToMinute(second) {
-    return addEmojiConverter(second / 60, second, "sec", "min");
+function dayToWeek(day) {
+    return addEmojiConverter(day / 7, day, "day", "week");
 }
 
-function hourToSecond(hour) {
-    return addEmojiConverter(hour * 3600, hour, "hour", "sec");
+function monthToDay(month) {
+    return addEmojiConverter(month * 30, month, "month", "day"); // Approximation
 }
 
-function secondToHour(second) {
-    return addEmojiConverter(second / 3600, second, "sec", "hour");
+function dayToMonth(day) {
+    return addEmojiConverter(day / 30, day, "day", "month"); // Approximation
 }
 
-function hourToMinute(hour) {
-    return addEmojiConverter(hour * 60, hour, "hour", "min");
+function yearToDay(year) {
+    return addEmojiConverter(year * 365, year, "year", "day"); // Approximation
 }
 
-function minuteToHour(minute) {
-    return addEmojiConverter(minute / 60, minute, "min", "hour");
+function dayToYear(day) {
+    return addEmojiConverter(day / 365, day, "day", "year"); // Approximation
 }
 
-function dayToSecond(days) {
-    return addEmojiConverter(days * 86400, days, "day", "sec");
+// 💾 Byte Conversions
+function gigabyteToMegabyte(gb) {
+    return addEmojiConverter(gb * 1000, gb, "GB", "MB");
 }
 
-function secondToDay(seconds) {
-    return addEmojiConverter(seconds / 86400, seconds, "sec", "day");
+function megabyteToGigabyte(mb) {
+    return addEmojiConverter(mb / 1000, mb, "MB", "GB");
 }
 
-function daysToMinute(days) {
-    return addEmojiConverter(days * 1440, days, "day", "min");
+function terabyteToGigabyte(tb) {
+    return addEmojiConverter(tb * 1000, tb, "TB", "GB");
 }
 
-function minuteToDay(minutes) {
-    return addEmojiConverter(minutes / 1440, minutes, "min", "day");
+function gigabyteToTerabyte(gb) {
+    return addEmojiConverter(gb / 1000, gb, "GB", "TB");
 }
 
-function dayToHour(day) {
-    return addEmojiConverter(day * 24, day, "day", "hour");
+function petabyteToTerabyte(pb) {
+    return addEmojiConverter(pb * 1000, pb, "PB", "TB");
 }
 
-function hourToDay(hour) {
-    return addEmojiConverter(hour / 24, hour, "hour", "day");
+function terabyteToPetabyte(tb) {
+    return addEmojiConverter(tb / 1000, tb, "TB", "PB");
 }
 
-// Temperature Conversions
+// 📏 Length Conversions
+function meterToKilometer(m) {
+    return addEmojiConverter(m / 1000, m, "m", "km");
+}
+
+function kilometerToMeter(km) {
+    return addEmojiConverter(km * 1000, km, "km", "m");
+}
+
+function centimeterToMeter(cm) {
+    return addEmojiConverter(cm / 100, cm, "cm", "m");
+}
+
+function meterToCentimeter(m) {
+    return addEmojiConverter(m * 100, m, "m", "cm");
+}
+
+function millimeterToCentimeter(mm) {
+    return addEmojiConverter(mm / 10, mm, "mm", "cm");
+}
+
+function centimeterToMillimeter(cm) {
+    return addEmojiConverter(cm * 10, cm, "cm", "mm");
+}
+
+function inchToCentimeter(inch) {
+    return addEmojiConverter(inch * 2.54, inch, "inch", "cm");
+}
+
+function centimeterToInch(cm) {
+    return addEmojiConverter(cm / 2.54, cm, "cm", "inch");
+}
+
+function footToMeter(ft) {
+    return addEmojiConverter(ft * 0.3048, ft, "ft", "m");
+}
+
+function meterToFoot(m) {
+    return addEmojiConverter(m / 0.3048, m, "m", "ft");
+}
+
+function mileToKilometer(mile) {
+    return addEmojiConverter(mile * 1.60934, mile, "mile", "km");
+}
+
+function kilometerToMile(km) {
+    return addEmojiConverter(km / 1.60934, km, "km", "mile");
+}
+
+// 🔥 Temperature Conversions
 function celsiusToKelvin(celsius) {
     return addEmojiConverter(celsius + 273.15, celsius, "°C", "K");
 }
@@ -90,84 +140,35 @@ function kelvinToFahrenheit(kelvin) {
     return addEmojiConverter((kelvin - 273.15) * 9/5 + 32, kelvin, "K", "°F");
 }
 
-// Byte Conversions
-function byteToBit(byte) {
-    return addEmojiConverter(byte * 8, byte, "B", "bit");
-}
-
-function bitToByte(bit) {
-    return addEmojiConverter(bit / 8, bit, "bit", "B");
-}
-
-function kilobyteToBit(kilobyte) {
-    return addEmojiConverter(kilobyte * 8000, kilobyte, "KB", "bit");
-}
-
-function bitToKilobyte(bit) {
-    return addEmojiConverter(bit / 8000, bit, "bit", "KB");
-}
-
-function kilobyteToByte(kilobyte) {
-    return addEmojiConverter(kilobyte * 1000, kilobyte, "KB", "B");
-}
-
-function byteToKilobyte(byte) {
-    return addEmojiConverter(byte / 1000, byte, "B", "KB");
-}
-
-function megabyteToByte(megabyte) {
-    return addEmojiConverter(megabyte * 1000000, megabyte, "MB", "B");
-}
-
-function byteToMegabyte(byte) {
-    return addEmojiConverter(byte / 1000000, byte, "B", "MB");
-}
-
-function megabyteToKiloByte(megabyte) {
-    return addEmojiConverter(megabyte * 1000, megabyte, "MB", "KB");
-}
-
-function kilobyteToMegabyte(kilobyte) {
-    return addEmojiConverter(kilobyte / 1000, kilobyte, "KB", "MB");
-}
-
-function megabyteToBit(megabyte) {
-    return addEmojiConverter(megabyte * 8000000, megabyte, "MB", "bit");
-}
-
-function bitToMegabyte(bit) {
-    return addEmojiConverter(bit / 8000000, bit, "bit", "MB");
-}
-
 module.exports = {
-    minuteToSeconds,
-    secondToMinute,
-    hourToSecond,
-    dayToSecond,
-    secondToDay,
-    secondToHour,
-    daysToMinute,
-    hourToMinute,
-    minuteToHour,
-    dayToHour,
-    byteToBit,
-    kilobyteToBit,
-    kilobyteToByte,
-    megabyteToByte,
-    byteToMegabyte,
-    megabyteToKiloByte,
-    kilobyteToMegabyte,
-    byteToKilobyte,
-    bitToKilobyte,
-    bitToByte,
-    fahrenheitToCelsius,
+    weekToDay,
+    dayToWeek,
+    monthToDay,
+    dayToMonth,
+    yearToDay,
+    dayToYear,
+    gigabyteToMegabyte,
+    megabyteToGigabyte,
+    terabyteToGigabyte,
+    gigabyteToTerabyte,
+    petabyteToTerabyte,
+    terabyteToPetabyte,
+    meterToKilometer,
+    kilometerToMeter,
+    centimeterToMeter,
+    meterToCentimeter,
+    millimeterToCentimeter,
+    centimeterToMillimeter,
+    inchToCentimeter,
+    centimeterToInch,
+    footToMeter,
+    meterToFoot,
+    mileToKilometer,
+    kilometerToMile,
+    celsiusToKelvin,
     kelvinToCelsius,
     celsiusToFahrenheit,
-    celsiusToKelvin,
+    fahrenheitToCelsius,
     fahrenheitToKelvin,
-    kelvinToFahrenheit,
-    hourToDay,
-    minuteToDay,
-    megabyteToBit,
-    bitToMegabyte
+    kelvinToFahrenheit
 };
